7f81b434a67c6d1db06fc964f07faf98b25c3444
Tyrel Chambers <tychambers3@gmail.com>
2020-06-30 19:17:17 -0400

    finishing

--- src/pages/ProfileSetup/Complete/Complete.jsx
+++ src/pages/ProfileSetup/Complete/Complete.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import { H1 } from '../../../components/Headings/Headings'
 import { MainButton } from '../../../components/Buttons/Buttons'
 import SetupProgress from '../../../components/SetupProgress/SetupProgress'
+import { useHistory } from 'react-router'
 
 function Complete() {
+  const history = useHistory();
+
+  const routeHandler = (e, route) => {
+    e.preventDefault()
+    history.push(route)
+  }
+
+
   return (
     <div className="container mx-auto">
       <H1 className="text-center">Congrats! Your profile is now all setup!</H1>
@@ -12,10 +21,13 @@ function Complete() {
       <div className="flex flex-col max-w-2xl bg justify-center shadow-lg mx-auto">
         <MainButton
           text="Take me to my dashboard!"
+          onClick={e => routeHandler(e, "/me")}
+
         />
         <hr/>
         <MainButton 
           text="Go back home!"
+          onClick={e => routeHandler(e, "/")}
         />
       </div>
     </div>
