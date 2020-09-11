e32e56a8e4ac8c1162aa464f4584d1f88d1530a2
Tyrel Chambers <tychambers3@gmail.com>
2020-09-11 18:25:08 -0400

    going to try storybook

--- src/components/ProjectWidget/ProjectWidget.jsx
+++ src/components/ProjectWidget/ProjectWidget.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { H3 } from "../Headings/Headings";
 import "./ProjectWidget.css";
 import { Link } from "react-router-dom";
 
@@ -19,24 +18,23 @@ function ProjectWidget({ project, tiny }) {
   }
 
   return (
-    <div className="flex flex-col widget-wrapper bg-indigo-700 box-shadow m-4">
+    <Link
+      to={`/project/${project.uuid}`}
+      className=" flex flex-col widget-wrapper bg-indigo-700 box-shadow m-4"
+    >
       <div className="widget-thumbnail-wrapper ">
         <img src={project.thumbnailUrl} alt="" className="widget-thumbnail" />
       </div>
 
-      <div className="widget-body flex justify-between ">
-        <H3>
-          <Link to={`/project/${project.uuid}`} className="text-white">
-            {project.title}
-          </Link>
-        </H3>
+      <div className="widget-body flex justify-between items-center ">
+        <p className="text-white text-xl font-bold m-0">{project.title}</p>
 
         <div className="flex items-center" style={{ width: "50px" }}>
           <i className="fas fa-fire-alt text-2xl mr-2 text-white"></i>
           <p className="text-white mt-2 font-bold">{project.likers.length}</p>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
